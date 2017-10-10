97f60fdf0e2bf0dccb28538220254078dbc65bd4
whitzhu <whitzhu@gmail.com>
2017-10-09 22:34:24 -0700

    add dashbaord component

---
+++ src/containers/DashboardComponent/DashboardComponent.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class DashboarComponent extends Component {
+  render() {
+    return (
+      <div>Dashboard Component</div>
+    );
+  }
+}
+
+export default DashboarComponent;
