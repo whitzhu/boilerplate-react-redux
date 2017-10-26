cb30af2806c554cc9ea5c6c3a6e8d37b333a4051
whitzhu <whitzhu@gmail.com>
2017-10-25 23:37:35 -0700

    integrate react-router-redux and redux-persist

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,28 +1,52 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+import { createStore, compose, applyMiddleware, combineReducers } from 'redux';
+import createHistory from 'history/createBrowserHistory';
+import { ConnectedRouter, routerReducer, routerMiddleware } from 'react-router-redux';
+import { persistStore, autoRehydrate } from 'redux-persist';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-import { BrowserRouter as Router } from 'react-router-dom';
 
-import reducer from '../../reducers';
+import reducers from '../../reducers';
 import Routes from '../../components/Routes/Routes';
 import './app.scss';
 
-const middlewares = [thunk, logger];
-const store = createStore(
-  reducer,
-  applyMiddleware(middlewares),
+const history = createHistory();
+const middleware = [thunk, logger, routerMiddleware(history)];
+const store = compose(autoRehydrate())(createStore)(
+  combineReducers({
+    ...reducers,
+    router: routerReducer,
+  }),
+  applyMiddleware(...middleware),
 );
 
-const App = () => (
-  <Provider store={store}>
-    <Router>
-      <div className="app-container">
-        <Routes />
-      </div>
-    </Router>
-  </Provider>
-);
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { rehydrated: false };
+  }
+
+  componentWillMount() {
+    persistStore(store, {}, () => {
+      this.setState({ rehydrated: true });
+    });
+  }
+
+  render() {
+    if (!this.state.rehydrated) {
+      return (<div>Loading...</div>);
+    }
+    return (
+      <Provider store={store}>
+        <ConnectedRouter history={history}>
+          <div className="app-container">
+            <Routes />
+          </div>
+        </ConnectedRouter>
+      </Provider>
+    );
+  }
+}
 
 export default App;
