f04a7b9eff13fb0e6feb4734943e31a362ed8ce9
whitzhu <whitzhu@gmail.com>
2017-10-25 23:25:28 -0700

    add new packages

--- server/index.js
+++ server/index.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const path = require('path');
 const favicon = require('serve-favicon');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
 
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 app.use(favicon(path.join(__dirname, '../public', 'favicon.ico')));
 app.use(express.static(path.join(__dirname, '../public')));
+
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, '/../public/index.html')));
 
 app.listen(process.env.PORT || 3000, () => {
   console.log(`Boilerplate-react-redux is listenting on PORT ${process.env.port || 3000}`);
