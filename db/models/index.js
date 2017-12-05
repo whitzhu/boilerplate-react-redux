ff800715b4fb483cf468850b85f8570e6f950ddd
whitzhu <whitzhu@gmail.com>
2017-12-04 22:24:07 -0800

    restructure db

--- db/models/index.js
+++ db/models/index.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
+
 const env = process.env.NODE_ENV || 'development';
 const config = require('../config.json')[env];
 
